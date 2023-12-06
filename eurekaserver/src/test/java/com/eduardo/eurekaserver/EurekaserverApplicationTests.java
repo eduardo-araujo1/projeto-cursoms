85e0750f427e1276b0755e13eb57922ec6093630
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-12-05 23:17:14 -0300

    first commit

---
+++ eurekaserver/src/test/java/com/eduardo/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mscloudgateway/src/test/java/com/eduardo/mscloudgateway/MccloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
