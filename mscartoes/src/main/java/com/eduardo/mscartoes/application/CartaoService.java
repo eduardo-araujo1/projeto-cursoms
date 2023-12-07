833544b6fbc10068c54f37edb9b6eea4b96abb1f
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-12-06 21:14:54 -0300

    Implementacao do metodo post

---
+++ mscartoes/src/main/java/com/eduardo/mscartoes/application/CartaoService.java
@@ -0,0 +1,26 @@
+package com.eduardo.mscartoes.application;
+
+import com.eduardo.mscartoes.domain.Cartao;
+import com.eduardo.mscartoes.infra.CartaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CartaoService {
+
+    private final CartaoRepository repository;
+    @Transactional
+    public Cartao save(Cartao cartao){
+        return repository.save(cartao);
+    }
+
+    public List<Cartao> getCartoesRendaMenorIgual(Long renda){
+        var rendaBigDecimal = BigDecimal.valueOf(renda);
+        return repository.findByRendaLessThanEqual(rendaBigDecimal);
+    }
+}
--- mscartoes/src/main/java/com/eduardo/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/com/eduardo/mscartoes/application/CartoesResource.java
@@ -1,16 +1,27 @@
 package com.eduardo.mscartoes.application;
 
+import com.eduardo.mscartoes.application.representation.CartaoSaveRequest;
+import com.eduardo.mscartoes.domain.Cartao;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cartoes")
 @RequiredArgsConstructor
 public class CartoesResource {
+
+    private final CartaoService service;
     @GetMapping
     public String status(){
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request){
+        Cartao cartao = request.toModel();
+        service.save(cartao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
